0d2f2c90f972f4c1424075a06df024bd398c3c54
Anthony Ingle <anthonyusa@me.com>
2021-11-25 01:07:18 -0500

    inital implementation

---
+++ components/Loader.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "../styles/Loader.module.css";
+
+export default function Loader() {
+  return <div className={styles.loader} />;
+}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
+  images: {
+    domains: ['images.unsplash.com']
+  },
   reactStrictMode: true,
+
 }
